3f6264e2120fdcf94e7ca1f4412258e49edfd9ed
Abhijit Sarkar <abhijitsarkar@Abhijits-MacBook-Air.local>
2026-05-26 16:54:27 +0530

    dockerized all services

--- db/db.sql
+++ db/db.sql
@@ -1,7 +1,7 @@
-// product service
-
 create database inventory;
 use inventory;
+
+
 create table products(
 	product_id bigint primary key auto_increment,
     product_name varchar(50) not null,
@@ -10,52 +10,40 @@ create table products(
     price decimal(10,2) not null,
     created_at timestamp default current_timestamp
 );
-select * from products;
-drop table products;
-desc products;
-insert into products(product_name, product_description, sku, price) values
-	("p1", "p1d", "2e233ew", 232.40);
-delete from products
-where product_id=1;
 
 
-
-
-// user service
-
-use inventory;
 create table users(
 	user_id bigint primary key auto_increment,
     username varchar(50) not null,
     email varchar(50) not null,
-    password varchar(50) not null
+    password varchar(200) not null
 );
-alter table users
-modify column password varchar(200) not null;
-desc users;
-select * from users;
+
+-- alter table users
+-- modify column password varchar(200) not null;
+-- desc users;
+
+
 create table roles(
 	role_id int primary key auto_increment,
     role_name varchar(50) not null
 );
-select * from roles;
 insert into roles(role_name) values 
 	("ROLE_USER"), ("ROLE_ADMIN"); 
+
+
 create table user_role(
 	user_id bigint not null,
     role_id int not null
 );
-select * from user_role;
 
-
-// order service
-
-use inventory;
 
 create table orders(
 	order_id bigint primary key auto_increment,
     total_price decimal(10,2) not null,
     user_id bigint not null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp,
     
     constraint fk_order_user
     foreign key (user_id)
@@ -63,11 +51,10 @@ create table orders(
     on delete cascade
 );
 
-alter table orders
-add column created_at timestamp default current_timestamp,
-add column updated_at timestamp;
+-- alter table orders
+-- add column created_at timestamp default current_timestamp,
+-- add column updated_at timestamp;
 
-desc orders;
 
 create table order_items(
 	order_item_id bigint primary key auto_increment,
@@ -88,13 +75,11 @@ create table order_items(
 );
 
 
-// inventory service
 create table skus(
 	sku_id varchar(50) primary key,
-    quantity int not null
+    quantity int not null,
+    version bigint not null default 1
 );
-
-select * from skus;
 
 insert into skus(sku_id, quantity) values 
 	("qpqpqp111qpqpqpq", 5),
